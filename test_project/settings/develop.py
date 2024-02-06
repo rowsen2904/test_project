60f6e773d4cf3f2084eb2b2ec2b170177370ea40
Rowsen <bayramovw0@gmail.com>
2024-02-06 22:01:53 +0500

    changed lifetime of tokens in develop mode

--- test_project/settings/develop.py
+++ test_project/settings/develop.py
@@ -8,6 +8,13 @@ ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+}
+
+
+
 # Django Debug Toolbar
 try:
     import debug_toolbar
