775aab731066df3198da4f0f38c46e2df85bd6b0
Rowsen <bayramovw0@gmail.com>
2024-02-06 16:29:13 +0500

    changed db from sqlite3 to postgresql

--- test_project/settings/base.py
+++ test_project/settings/base.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_NAME", "test_project"),
+        'USER': os.getenv("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "postgres"),
+        'HOST': os.getenv("POSTGRES_HOST", "localhost"),
+        'PORT': os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
