f7456587d8f0d526724f2220e7c5728e92a78488
Rowsen <bayramovw0@gmail.com>
2024-02-06 18:50:49 +0500

    created refferal model, added new method of user

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,35 @@
+from uuid import uuid4
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    referral_code = models.CharField(
+        max_length=255,
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    def create_new_referral_code(self):
+        self.referral_code = uuid4().hex
+        self.save()
+
+    def get_count_of_referrals(self):
+        print(self.referrals)
+
+
+class Referral(models.Model):
+    inviter = models.ForeignKey(
+        User,
+        related_name='referrals',
+        on_delete=models.CASCADE
+    )
+    invited = models.OneToOneField(
+        User,
+        related_name='referral',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
