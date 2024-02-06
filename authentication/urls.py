9b6968431318615fed128689a184833d2f911c35
Rowsen <bayramovw0@gmail.com>
2024-02-06 19:42:49 +0500

    added registration

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,8 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+from . import views
+
 
 urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import RegisterSerializer
+from profiles.models import User
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        user = super().post(request, *args, **kwargs)
+        referral_code = user.data.get("referral_code")
+        if referral_code is not None:
+            try:
+                inviter = User.objects.get(referral_code=referral_code)
+                invited = User.objects.get(username=user.data.get("username"))
+            except User.DoesNotExist:
+                pass
+            else:
+                inviter.set_new_referral(invited)
+
+        return user
