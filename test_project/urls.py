89b348810188ac54e7f68ab8f8d81c93664ceeb0
Rowsen <bayramovw0@gmail.com>
2024-02-06 18:02:05 +0500

    modified test_project/urls.py file

--- test_project/urls.py
+++ test_project/urls.py
@@ -26,9 +26,12 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/', include('authentication.urls')),
+    path('api/v1/', include('profiles.urls')),
+
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
