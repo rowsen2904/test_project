8be11cfce0966d450d2689ddc73e9a89ab53adc7
Rowsen <bayramovw0@gmail.com>
2024-02-06 16:16:41 +0500

    added django-debug-toolbar

--- test_project/settings/develop.py
+++ test_project/settings/develop.py
@@ -6,3 +6,16 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+# Django Debug Toolbar
+try:
+    import debug_toolbar
+
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+    INTERNAL_IPS = [
+        "127.0.0.1"
+    ]
+except ModuleNotFoundError:
+    pass
--- test_project/urls.py
+++ test_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -29,3 +30,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls")),]
