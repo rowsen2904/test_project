7f945ad31df91540bd21f43ce71dbee4c42db975
Rowsen <bayramovw0@gmail.com>
2024-02-06 20:41:01 +0500

    added EMAIL constants

--- test_project/settings/base.py
+++ test_project/settings/base.py
@@ -119,6 +119,16 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'profiles.User'
 
 
+# Email
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+
 # Simple JWT settings
 
 SIMPLE_JWT = {
