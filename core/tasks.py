83d7ed1bc066c06cfd5a3c2b703d48a8bc00a794
Rowsen <bayramovw0@gmail.com>
2024-02-06 22:24:19 +0500

    modified core/tasks.py file

--- core/tasks.py
+++ core/tasks.py
@@ -1,16 +1,15 @@
 from typing import List
 
 from celery import shared_task
-from django.conf import settings
 from django.core.mail import send_mail
 
 
 @shared_task
-def send_email(subject: str, message: str, recipient_list: List[str]) -> None:
+def send_email(subject: str, message: str, from_email: str, recipient_list: List[str]) -> None:
     send_mail(
         subject=subject,
         message=message,
-        from_email=settings.EMAIL_HOST_USER,
+        from_email=from_email,
         recipient_list=recipient_list
     )
     print("Email sent!")
