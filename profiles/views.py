ff6eceb347d54b4813fc4da030139cc22ef5d99a
Rowsen <bayramovw0@gmail.com>
2024-02-06 22:25:31 +0500

    email sender has been done asynchronously

--- profiles/views.py
+++ profiles/views.py
@@ -4,18 +4,20 @@ from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from core.tasks import send_email
+
 
 class CreateReferralCodeAPIView(GenericAPIView):
     permission_classes = (IsAuthenticated, )
+    email_subject = 'New Referral Code'
+    from_email = settings.EMAIL_HOST_USER
 
     def get(self, request, *args, **kwargs):
         referral_code = request.user.create_new_referral_code()
-        sender = settings.EMAIL_HOST_USER
-        send_mail(
-            'New Referral Code',
-            f'Your new referral code is: {referral_code}',
-            sender,
-            ["bayrumovw@gmail.com"],
-            fail_silently=False
+        send_email.delay(
+            subject=self.email_subject,
+            message=f'Your new referral code is: {referral_code}',
+            from_email=self.from_email,
+            recipient_list=[request.user.email]
         )
         return Response({'referral_code': referral_code}, status=200)
