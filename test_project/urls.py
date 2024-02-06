871cd873d73be3681c39410722c9886271363d67
Rowsen <bayramovw0@gmail.com>
2024-02-06 18:06:25 +0500

    urls of token login replaced

--- test_project/urls.py
+++ test_project/urls.py
@@ -17,21 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('profiles.urls')),
-
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
