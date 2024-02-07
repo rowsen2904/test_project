1beb255617b2b0d56a4556e2e031cb4ab8c7b91d
Rowsen <bayramovw0@gmail.com>
2024-02-07 14:22:53 +0500

    added spectacular view (Swagger documentation)

--- test_project/urls.py
+++ test_project/urls.py
@@ -17,12 +17,24 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('profiles.urls')),
+
+    # Swagger URLs
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/v1/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
