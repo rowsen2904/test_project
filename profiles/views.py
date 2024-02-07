dbb1d27f5437a5b8796f34422f77b3d54bf213e7
Rowsen <bayramovw0@gmail.com>
2024-02-07 09:16:23 +0500

    removed unused package

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
